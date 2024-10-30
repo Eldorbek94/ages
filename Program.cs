a826c0d7b04a68f702f94d3a952e5cef4d3730cc
Eldorbek94 <eldorbtc@gmail.com>
2024-10-30 08:40:14 +0500

    Iinfra: Upload Project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿
+Console.WriteLine("Hello enter your age :");
+string age = Console.ReadLine();
+string greeting  = $"Hello Mr.Eldor your age is {age}";
+Console.WriteLine(greeting);
+
+Console.WriteLine("Hello Enter another age :");
+string age2 = Console.ReadLine();
+int dif = Convert.ToInt32(age)-Convert.ToInt32(age2);
+Console.WriteLine($"Difference: {dif}");
+
+
